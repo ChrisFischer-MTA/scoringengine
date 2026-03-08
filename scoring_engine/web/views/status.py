37b6c2533f012e53fb71759959f9a7ad1a6dcf77
Paola Bayona <bayonapaola2002@gmail.com>
2026-03-08 09:55:27 -0400

    fix: restandardize setting names

--- scoring_engine/web/views/status.py
+++ scoring_engine/web/views/status.py
@@ -14,8 +14,8 @@ def status():
     elif current_user.is_blue_team and Setting.get_setting('blue_team_view_status_page').value is True:
         return render_template(
             "status.html",
-            current_enabled=Setting.get_setting('blue_team_view_status_current').value,
-            historical_enabled=Setting.get_setting('blue_team_view_status_historical').value,
+            current_enabled=Setting.get_setting('blue_team_view_current_status').value,
+            historical_enabled=Setting.get_setting('blue_team_view_historical_status').value,
         )
     else:
         return redirect(url_for("auth.unauthorized"))
